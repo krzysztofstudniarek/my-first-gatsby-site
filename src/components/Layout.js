758811bd2db4c26a786173648f2efeac67c3d2af
Krzysztof Studniarek <krzysztofstudniarek@MacBook-Pro-Admin.local>
2021-12-02 21:18:40 +0100

    adding styled components and creating the Top component

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div>
+        <title>{pageTitle}</title>
+        {children}
+    </div>
+  )
+}
+
+export default Layout;
