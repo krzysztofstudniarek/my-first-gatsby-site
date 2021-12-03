211673b008102e6b4bca3d54a4266d2e0c8b9a2a
Krzysztof Studniarek <krzysztofstudniarek@MacBook-Pro-Admin.local>
2021-12-03 09:51:57 +0100

    seting logo to 2x size

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,6 +7,7 @@ const Logo = styled.div`
     background-image: url(${img});
     background-repeat: no-repeat;
     background-position: center;
+    background-size: 100%;
     left: 0px;
     top:0px;
     width: 280px;
