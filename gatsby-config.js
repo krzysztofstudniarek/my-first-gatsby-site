7ad59d5111811aad81458583089ee78d9326f084
Krzysztof Studniarek <krzysztofstudniarek@MacBook-Pro-Admin.local>
2022-03-02 16:19:53 +0100

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  proxy: {
+    prefix: "/data",
+    url: "http://ec2-18-223-120-80.us-east-2.compute.amazonaws.com",
+  },
   siteMetadata: {
     siteUrl: "http://demaskujemydezinformacje.pl",
     title: "Demaskujemy Dezinformację",
